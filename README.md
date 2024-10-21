412e04e15da48cb674e2226840d5149f1c13f721
dmail-network <dmailnetworkcontact@gmail.com>
2024-10-21 23:27:10 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# dmailv2
+
+Dmail mailbox V2
+
+## before
+### install dfx and run dfx
+- install: `sh -ci "$(curl -fsSL https://sdk.dfinity.org/install.sh)"`
+- see version: `dfx --version`
+- enter root: `cd dmail`
+- run: `dfx start`
+- run: `dfx deploy`
+
+## Available Scripts
+
+In the project directory, you can run:
+
+### develop
+`yarn start`
+
+### production
+`yarn build`
---
+++ config-overrides.js
@@ -0,0 +1,43 @@
+const { override, overrideDevServer, fixBabelImports, addWebpackAlias, addWebpackModuleRule, setWebpackOptimizationSplitChunks, addBundleVisualizer } = require('customize-cra');
+
+const path = require('path')
+const resolve = dir => path.join(__dirname, '.', dir)
+
+const removeManifest = () => config => {
+  config.plugins = config.plugins.filter(
+      p => p.constructor.name !== "ManifestPlugin"
+  );
+  return config;
+};
+
+const addProxy = () => (configFunction) => {
+  // configFunction.proxy = {
+  //     '/api/': {
+  //         target: 'https://www.api.com',
+  //         changeOrigin: true,
+  //         pathRewrite: { '^/api': '/' },
+  //     },
+  // };
+
+  return configFunction;
+}
+
+const closedMap = config => {
+   config.devtool = config.mode === 'development' ? 'cheap-module-source-map' : false;
+   return config;
+ }; 
+
+module.exports = {
+  webpack: override(
+      addBundleVisualizer({}, true),
+      removeManifest(),
+      addWebpackAlias({
+        ['@']: resolve('src')
+      }),
+      closedMap
+      // addLessLoader(),
+  ),
+  devServer: overrideDevServer(
+    addProxy()
+  )
+}
